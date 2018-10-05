2778a314aada8620776907c861fd243433271743
rumor1993 <rumor1993@naver.com>
2018-10-05 08:46:21 -0700

    Updates

---
+++ precache-manifest.57269a71e7a4bd08126b6c9be0df27da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8467066b35fa91fc044",
+    "url": "/Calculator_app/static/js/runtime~main.e8467066.js"
+  },
+  {
+    "revision": "efe7c1b476475643f09d",
+    "url": "/Calculator_app/static/js/main.efe7c1b4.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/Calculator_app/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "efe7c1b476475643f09d",
+    "url": "/Calculator_app/static/css/main.f202ae3d.chunk.css"
+  },
+  {
+    "revision": "8071feef3df007877a6f363c5a81385a",
+    "url": "/Calculator_app/index.html"
+  }
+];
